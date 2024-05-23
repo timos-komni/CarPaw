e8461557ea5e6189dc747f48419fa9d9adf72e35
chloe <anastasia.tsarcho@gmail.com>
2024-05-23 11:53:52 +0300

    SplashScreen

--- app/src/main/java/com/timkom/carpaw/MainActivity.kt
+++ app/src/main/java/com/timkom/carpaw/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -45,6 +46,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.timkom.carpaw.ui.theme.CarPawTheme
 import com.timkom.carpaw.ui.theme.Purple80
 
@@ -53,6 +55,7 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             CarPawTheme(dynamicColor = false) {
